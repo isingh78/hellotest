41259e4b1926110693bfad130a902fae48e178dd
Randhawa <Inderpal.Randhawa@aristocrat.com>
2020-04-29 14:15:13 +0530

    Test repo

---
+++ CentralTestRepo/src/main/java/io/github/isingh78/App.java
@@ -0,0 +1,13 @@
+package io.github.isingh78;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
